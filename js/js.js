71e66f5fa1b2d8e8f971d8e0affcb85c815a80af
wuhao14 <wuhao14@meituan.com>
2022-03-24 16:26:17 +0800

    Site updated: 2022-03-24 16:26:17

--- js/js.js
+++ js/js.js
@@ -1,18 +1,9 @@
 var web_style = $("#web_style").val();
-var valine_appid = $("#valine_appid").val();
-var valine_appKey = $("#valine_appKey").val();
-
-new Valine({
-    el: '#vcomments',
-    appId: valine_appid,
-    appKey: valine_appKey,
-    placeholder: '请输入内容...',
-    avatar: "wavatar"
-})
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre').forEach((block) => {
         hljs.highlightBlock(block);
+        // hljs.lineNumbersBlock(block);
     });
 });
 
@@ -52,7 +43,7 @@ if (getCookie("style") == null) {
     updateStyle();
 }
 
-$("#update_style").change(function() {
+$("#update_style").change(function () {
     var style = $("#update_style").is(':checked');
     if (style) {
         setCookie("style", "black")
